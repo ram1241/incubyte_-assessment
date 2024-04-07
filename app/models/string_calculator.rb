c00593540f5474789bdd00b556cc9b9db7ab7910
pramakrishna <pothala.ramakrishna@plansource.com>
2024-04-07 18:01:21 +0530

    added-negative-numbers-case-code

--- app/models/string_calculator.rb
+++ app/models/string_calculator.rb
@@ -1,12 +1,38 @@
 class StringCalculator
+  def initialize
+    @default_delimiters = [",", "\n", ";"]
+  end
   def add(*inputs)
     default_delimiters = [",", "\n"]
-    inputs= inputs.reject(&:empty?)
+    return "please enter inputs" if inputs.empty?
+    negatives = []
     inputs.map do |numbers|
-      numbers =  numbers.split(Regexp.union(default_delimiters)).map(&:to_i)
+      delimiters, numbers = custom_delimiters(numbers)
+      numbers = split_numbers(numbers, delimiters)
+      negatives << numbers.select { |num| num < 0 }
       numbers.sum
     end
+    negatives.flatten!
+    return "negative numbers not allowed: #{negatives.join(', ')}" unless negatives.empty?
   end
+
+  private
+
+  def custom_delimiters(numbers)
+    if numbers.start_with?("//")
+      numbers = numbers[2..-1]
+      delimiter, numbers = numbers.split("\n", 2)
+      delimiters = delimiter.chars
+    else
+      delimiters = @default_delimiters
+    end
+    [delimiters, numbers]
+  end
+  def split_numbers(numbers, delimiters)
+    numbers.split(Regexp.union(delimiters)).map(&:to_i)
+  end
+
 end
 calculator = StringCalculator.new
-p calculator.add("", "1", "1,5")
+p calculator.add("-2", "-3")
+
